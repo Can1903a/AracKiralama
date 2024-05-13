dae091f6cea8629a06f3fd645a3703bb8740f07f
Can1903a <atlasis_3131@hotmail.com>
2024-05-13 16:19:48 +0300

    aaa

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ if (isset($_SESSION['Kullanici_id'])) {
                 </div>
                 <div class="form-group">
                     <label for="baslangic_tarihi">Başlangıç Tarihi:</label>
-                    <input type="text" id="baslangic_tarihi" name="baslangic_tarihi"  class="form-control" required>
+                    <input type="text" id="baslangic_tarihi" name="baslangic_tarihi"  class="form-control" readonly="readonly" required>
                 </div>
                 <div class="form-group">
                     <label for="bitis_tarihi">Bitiş Tarihi:</label>
